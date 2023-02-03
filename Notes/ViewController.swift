6f70ac00faff1244f5755fd60b744f7790b88743
Victor Borisovsky <vyuborisovskiy@stud.eltech.ru>
2023-02-03 15:11:58 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Виктор Борисовский on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
